c8fcc5c7969f9b793dba7186e6da009d8383816e
Kevin Nourian <knourian@outlook.com>
2021-10-17 15:24:13 +0300

    Corrected Docstrings

--- Calculator/Calculator_Class.py
+++ Calculator/Calculator_Class.py
@@ -4,34 +4,34 @@ class Calculator:
         self.memory = memory
 
     def add(self, num):
-        '''Adds an integer to number in the memory.'''
+        """Adds an integer to number in the memory."""
         self.memory = self.memory + num
 
     def sub(self, num):
-        '''Subtracts an integer to number in the memory.'''
+        """Subtracts an integer to number in the memory."""
         self.memory = self.memory - num
 
     def mult(self, num):
-        '''Adds an integer to number in the memory.'''
+        """Adds an integer to number in the memory."""
         self.memory = self.memory * num
 
     def div(self, num):
-        '''Divides the number in memory by an integer.'''
+        """Divides the number in memory by an integer."""
         self.memory = self.memory / num
 
     def root(self, root):
-        '''Takes the (n) root of the number in the memory.'''
+        """Takes the (n) root of the number in the memory."""
         self.memory = self.memory**(1/root)
 
     def get_result(self):
-        '''Returns the memory content.'''
+        """Returns the memory content."""
         return self.memory
 
     def clear_memory(self):
-        '''Sets the memory to 0.'''
+        """Sets the memory to 0."""
         self.memory = 0
         return self.memory
 
     def set_memory(self, num):
-        '''Sets the memory to the integer provided by user.'''
+        """Sets the memory to the integer provided by user."""
         self.memory = num
